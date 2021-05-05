952673ddbe48ec89963123dc7f0815ee110e4261
elberth.brandao <zelltr@hotmail.com>
2021-05-05 14:13:01 -0300

    first commit

---
+++ clientes/src/main/java/com/elberthbrandao/clientes/model/repository/ServicoRepository.java
@@ -0,0 +1,9 @@
+package com.elberthbrandao.clientes.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.elberthbrandao.clientes.model.entity.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer>{
+
+}
---
+++ clientes/src/main/java/com/elberthbrandao/clientes/rest/ClienteController.java
@@ -0,0 +1,45 @@
+package com.elberthbrandao.clientes.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.elberthbrandao.clientes.model.entity.Cliente;
+import com.elberthbrandao.clientes.model.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+	private final ClienteRepository repository;
+
+	@Autowired
+	public ClienteController(ClienteRepository repository) {
+		this.repository = repository;
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente salvar(@RequestBody Cliente cliente) {
+		return repository.save(cliente);
+	}
+
+	@GetMapping("{id}")
+	public Cliente acharPorId(@PathVariable Integer id) {
+		return repository
+				.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	public void deletar(@PathVariable Integer id) {
+		
+	}
+
+}
