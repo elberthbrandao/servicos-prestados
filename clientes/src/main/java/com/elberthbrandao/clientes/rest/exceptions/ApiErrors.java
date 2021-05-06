46f8349eb84f92ef0326b62578cbd058768658c8
elberth.brandao <zelltr@hotmail.com>
2021-05-06 14:43:37 -0300

    Tratamento de errors da Api de Clientes

---
+++ clientes/src/main/java/com/elberthbrandao/clientes/rest/exceptions/ApiErrors.java
@@ -0,0 +1,22 @@
+package com.elberthbrandao.clientes.rest.exceptions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.Getter;
+
+public class ApiErrors {
+
+	@Getter
+	List<String> errors;
+	
+	public ApiErrors(List<String> errors) {
+		this.errors = errors;
+	}
+	
+	public ApiErrors(String message) {
+		this.errors = Arrays.asList(message);
+	}
+	
+}
+
