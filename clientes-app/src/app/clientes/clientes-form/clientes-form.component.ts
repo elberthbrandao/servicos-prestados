9031288eda9704946b562f1d75d93d7a1a0d6943
elberth.brandao <zelltr@hotmail.com>
2021-05-23 02:54:05 -0300

    Populando cliente através do formulário

--- clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -10,9 +10,14 @@ export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
 
-  constructor() {}
+  constructor() {
+    this.cliente = new Cliente();
+  }
 
   ngOnInit(): void {
   }
 
+  clicar(){
+    console.log(this.cliente);
+  }
 }
