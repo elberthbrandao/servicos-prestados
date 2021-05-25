4d253ff7bf556aa325bbe0770b52121b8f4f0722
elberth.brandao <zelltr@hotmail.com>
2021-05-25 15:56:28 -0300

    Atualizar cliente

--- clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Cliente } from '../cliente';
 import { ClientesService } from '../../clientes.service';
 
@@ -13,28 +13,54 @@ export class ClientesFormComponent implements OnInit {
   cliente: Cliente;
   success: boolean = false;
   errors: String[];
+  id: number;
 
   constructor(
     private service: ClientesService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) {
     this.cliente = new Cliente();
   }
 
   ngOnInit(): void {
+    let params: Params = this.activatedRoute.params;
+    if(params && params.value && params.value.id){
+      this.id = params.value.id;
+
+      this.service.getClienteById(this.id).subscribe(
+        response => this.cliente = response,
+        erroResponse => this.cliente = new Cliente()
+      )
+    }
   }
 
   onSubmit() {
-    this.service.salvar(this.cliente).subscribe(
-      response => {
-        this.success = true;
-        this.errors = null;
-        this.cliente = response;
-      },
-      errorResponse => {
-        this.success = false;
-        this.errors = errorResponse.error.errors;
-      })
+    if(this.id){ 
+      console.log(this.id)
+      console.log(this.cliente)
+      this.service.atualizar(this.cliente).subscribe(
+        response => {
+          this.success = true;
+          this.errors = null;
+        },
+        errorResponse => {
+          this.success = false;
+          this.errors = errorResponse.error.errors;
+        })
+
+    } else {
+      this.service.salvar(this.cliente).subscribe(
+        response => {
+          this.success = true;
+          this.errors = null;
+          this.cliente = response;
+        },
+        errorResponse => {
+          this.success = false;
+          this.errors = errorResponse.error.errors;
+        })
+    }
   }
 
   voltarParaListagem(){
