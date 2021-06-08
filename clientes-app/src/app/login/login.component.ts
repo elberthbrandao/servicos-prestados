84929183dd315353e1c64677527f4662c0f11db7
elberth.brandao <zelltr@hotmail.com>
2021-06-08 20:47:51 -0300

    Cadastro de usuário no front-end

--- clientes-app/src/app/login/login.component.ts
+++ clientes-app/src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { AuthService } from '../auth.service';
+import { Usuario } from './usuario';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,9 +15,11 @@ export class LoginComponent {
   password: string;
   loginError: boolean;
   cadastrando: boolean;
+  mensagemSucesso: string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   onSubmit(){
@@ -24,6 +29,22 @@ export class LoginComponent {
   preparaCadastrar(event){
     event.preventDefault();
     this.cadastrando = true;
+  }
+
+  cadastrar(){
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+
+    this.authService.salvar(usuario).subscribe(
+      response => {
+        this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login.";
+        this.loginError = false;
+      },
+      error => {
+        this.loginError = true;
+      }
+    )
   }
 
   cancelaCadastro() {
